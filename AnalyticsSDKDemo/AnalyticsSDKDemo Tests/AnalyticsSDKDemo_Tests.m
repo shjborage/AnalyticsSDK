f40f9fd96480958c9bbd9fb94f992e55fa9a76a7
Eric <shjborage@gmail.com>
2014-05-07 11:08:54 +0800

    Prepare for unit test

---
+++ AnalyticsSDKDemo/AnalyticsSDKDemo Tests/AnalyticsSDKDemo_Tests.m
@@ -0,0 +1,34 @@
+//
+//  AnalyticsSDKDemo_Tests.m
+//  AnalyticsSDKDemo Tests
+//
+//  Created by shihaijie on 5/6/14.
+//  Copyright (c) 2014 Saick. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AnalyticsSDKDemo_Tests : XCTestCase
+
+@end
+
+@implementation AnalyticsSDKDemo_Tests
+
+- (void)setUp
+{
+  [super setUp];
+  // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+  // Put teardown code here. This method is called after the invocation of each test method in the class.
+  [super tearDown];
+}
+
+- (void)testExample
+{
+  XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
